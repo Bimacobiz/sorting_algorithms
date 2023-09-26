a571416a74c10eebe1af548d00059757f6ce8b74
Bimacobiz <collinsmahigi@gmail.com>
2023-09-26 08:47:41 -0700

    changes

--- deck.h
+++ deck.h
@@ -15,6 +15,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+bool isSorted(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
